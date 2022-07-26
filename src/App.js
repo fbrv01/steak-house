0d79c2f0dd0edb5d155f873393e72e1c6189f3b5
fbrv01 <fbucholc@gmail.com>
2022-07-26 13:58:06 +0200

    first desktop version, not yet responsive, no slider yet

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Contact from './components/contact/Contact';
+import Navbar from './components/nav/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
